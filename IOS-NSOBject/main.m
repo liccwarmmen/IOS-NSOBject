f13c75673f8fb286ab58bee8e765fe474a5c6c00
lcc <lcc@lccdeMac.local>
2015-07-22 00:33:25 -0700

    Initial Commit

---
+++ IOS-NSOBject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IOS-NSOBject
+//
+//  Created by lcc on 15-7-22.
+//  Copyright (c) 2015年 lcc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
